d80996f5f876eaf886f5f247bc3e35807629e41e
Your Name <you@example.com>
2021-04-09 20:48:13 +0000

    Maven setup

---
+++ webapp/src/main/java/leon/schenk/App.java
@@ -0,0 +1,13 @@
+package leon.schenk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
